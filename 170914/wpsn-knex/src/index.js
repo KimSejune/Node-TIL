79ca7830249c674c8f7a4d7cee058deac61b7b34
kimsejune <tpwns1088@gmail.com>
2017-09-15 13:11:46 +0900

    url 추가기능 구현

--- 170914/wpsn-knex/src/index.js
+++ 170914/wpsn-knex/src/index.js
@@ -57,6 +57,28 @@ app.post('/logout', (req,res) => {
   res.redirect('/login')
 })
 
+// authMiddleware를 사용하지 않는다면 req.user에 아무런 정보가 들어가지 않는다.
+app.post('/url_entry', authMiddleware, urlencodedMiddleware, (req, res) => {
+  const long_url = req.body.long_url
+  // 데이터 저장
+  query.createUrlEntry(long_url, req.user.id)
+    .then( () => {
+      res.redirect('/')
+    })
+})
+
+app.get('/:id', (req, res, next) => {
+  query.getUrlById(req.params.id)
+    .then(urlEntry => {
+      if(urlEntry){
+        res.redirect(urlEntry.long_url) // 301 moved~~ 영원히 이동한다 (브라우저에 저장) , 302 브라우저에 저장안하고 다시보낸다.
+        // res.redirect(301, entry.long_url)를 해야하지만 사람들이 얼마나 클릭했는지 확인하기 위해서 301을 뺐다.
+      }else {
+        next()
+      }
+    })
+})
+
 app.listen(3000, () => {
   console.log('listening...')
 })
